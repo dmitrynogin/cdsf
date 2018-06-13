da3e9a746fbcc9ce831e4cc2c91fffcf378eed25
Dmitry Nogin <dmitryn@mail.com>
2018-06-13 12:02:46 -0700

    Extend Folder, FolderFile API

--- Cds.Folders/Folder.cs
+++ Cds.Folders/Folder.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Linq;
 
 namespace Cds.Folders
 {
@@ -19,11 +20,11 @@ namespace Cds.Folders
 
         public Folder AsRoot() => new Folder(FullPath, FullPath);
 
-        public Folder Down(string folder) => 
-            new Folder(FullPath + folder, Root);
-
         public Folder Up() =>
             new Folder(FullPath.Parent, Root);
+
+        public Folder Down(string folder) => 
+            new Folder(FullPath + folder, Root);
 
         public bool Exists =>
             Directory.Exists(FullPath);
@@ -33,5 +34,13 @@ namespace Cds.Folders
 
         public FolderFile File(OSPath path) =>
             new FolderFile(FullPath + path, Root);
+
+        public IEnumerable<Folder> Folders() =>
+            from fullPath in Directory.EnumerateDirectories(FullPath)
+            select new Folder(fullPath, Root);
+
+        public IEnumerable<FolderFile> Files() =>
+            from fullPath in Directory.EnumerateFiles(FullPath)
+            select new FolderFile(fullPath, Root);
     }
 }
--- Cds.Folders/FolderFile.cs
+++ Cds.Folders/FolderFile.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Cds.Folders
 {
     public class FolderFile : FolderEntry
     {
-        public FolderFile(OSPath fullPath, string root = "")
+        public FolderFile(OSPath fullPath)
+            : this(fullPath, OSPath.Empty)
+        {
+        }
+
+        public FolderFile(OSPath fullPath, OSPath root)
             : base(fullPath, root)
         {            
         }
+
+        public bool Exists => File.Exists(FullPath);
     }
 }
