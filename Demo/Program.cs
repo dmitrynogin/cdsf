c29935d8ec948ac21faccf574527938d45a6a41a
Dmitry Nogin <dmitryn@mail.com>
2018-06-13 08:16:53 -0700

    Add demo project

---
+++ Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using Cds.Folders;
+using System;
+using static System.Console;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OSPath path = @"/foo\bar.txt";
+            
+            // Windows output
+            WriteLine(path);            // \foo\bar.txt
+            WriteLine(path.Windows);    // \foo\bar.txt
+            WriteLine(path.Unix);       // /foo/bar.txt
+
+            // MacOS output
+            WriteLine(path);            // /foo/bar.txt
+            WriteLine(path.Windows);    // \foo\bar.txt
+            WriteLine(path.Unix);       // /foo/bar.txt
+
+            // It also helpes converting between relative and absolute paths:
+
+            OSPath ap = "/foo/bar";
+            WriteLine(ap.Relative.Unix); // foo/bar
+            WriteLine(ap.Absolute.Unix); // /foo/bar
+
+            OSPath rp = "/foo/bar";
+            WriteLine(rp.Relative.Unix); // foo/bar
+            WriteLine(rp.Absolute.Unix); // /foo/bar
+
+            // And to perform path arithmetics (Windows output):
+            OSPath root = @"/foo\bar";
+            WriteLine(root + "file.txt");        // \foo\bar\file.txt
+            WriteLine(root + "file.txt" - root); // file.txt
+        }
+    }
+}
